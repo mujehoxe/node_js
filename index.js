3878f1fec1bcfac0da8c60f26d069884dc4c711c
muje hoxe <mujehoxe@gmail.com>
2023-12-28 19:59:24 +0100

    post products working

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
 server.listen(3000, () => {
   console.log("Server started on port: 3000");
 });
 
-const products = [
-  {
-    id: 1,
-    name: "chambra",
-  },
-  {
-    id: 2,
-    name: "shampoo",
-  },
-];
-
 server.get("/", (req, res) => {
   fs.readFile("index.html", "utf8", (err, html) => {
     fs.readFile("data.txt", "utf8", (err, data) => {
-      console.log(data);
       res.send(html + data);
     });
   });
 });
 
 server.get("/product", (req, res) => {
-  res.send(products[req.query.id - 1]);
+  fs.readFile("products.json", "utf8", (err, data) => {
+    const products = JSON.parse(data);
+    res.contentType("application/json");
+    res.send(products[req.query.id - 1]);
+  });
 });
 
+function readProductsAndRespond(res) {
+  fs.readFile("products.json", "utf8", (err, data) => {
+    res.contentType("application/json");
+    res.send(data);
+  });
+}
+
 server.get("/products", (req, res) => {
-  res.send(products);
+  readProductsAndRespond(res);
+});
+
+server.post("/products", (req, res) => {
+  console.log(req.body);
+  const product = { name: req.body["product-name"] };
+
+  fs.readFile("products.json", "utf8", (err, data) => {
+    const products = JSON.parse(data);
+
+    if (products[products.length - 1].name === product.name) {
+      res.status(401).send("product already exists");
+      return;
+    }
+
+    product.id = products.length + 1;
+    products.push(product);
+    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+      if (err) console.log("Error writing file:", err);
+    });
+    res.send(product);
+  });
 });
 
 server.get("/employees", (req, res) => {
